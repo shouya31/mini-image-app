b9fe4a36e3a9f0e11f6da14d88a65d3c676f733b
shouya0331 <shouya0331@ezweb.ne.jp>
2020-07-19 14:32:42 +0900

    生成したsrcを用いてプレビューを挿入しましょう

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -2,5 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
   document.querySelector('#message_image'),addEventListener('change', function(e){
     const preview = e.target.files[0];
     const blob = window.URL.createObjectURL(preview);
+    const html = `<img src="${blob}">`;
+    document.querySelector('form').insertAdjacentHTML('afterend' , html);
   });
 });
