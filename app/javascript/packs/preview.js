5b56c498cf90d3f3a5d2f73f97863e6039d904a8
shouya0331 <shouya0331@ezweb.ne.jp>
2020-07-19 14:33:56 +0900

    表示されている画像を削除しましょう

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
   document.querySelector('#message_image'),addEventListener('change', function(e){
+    const image_content = document.querySelector('img');
+    if (image_content){
+      image_content.remove();
+    };
+
     const preview = e.target.files[0];
     const blob = window.URL.createObjectURL(preview);
     const html = `<img src="${blob}">`;
