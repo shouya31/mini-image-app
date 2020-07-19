0e3499edb0a30f4c923adf97b1a5fe718c18f550
shouya0331 <shouya0331@ezweb.ne.jp>
2020-07-19 15:46:59 +0900

    label要素をクリックしたら関数が動くようにしましょう

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,15 +1,26 @@
-document.addEventListener('DOMContentLoaded', function(){
-  document.querySelector('#message_image'),addEventListener('change', function(e){
-  const image_content = document.querySelector('img');
-    if (image_content){
-      image_content.remove();
-    };
+document.addEventListener('DOMContentLoaded', function (){
+  const clickLabel = function(){
+    const inputAll = document.getElementsByName('message[images][]');
+    const inputLast = inputAll[inputAll.length -1];
+    const inputId = inputLast.getAttribute('id');
+    console.log(inputId);
+    document.getElementById('click-upload').setAttribute('for', inputId);
+  };
 
-    const preview = e.target.files[0];
-    const blob = window.URL.createObjectURL(preview);
-    const html = `<img src="${blob}">`;
-    const inputHTML = `<input type='file' name='message[images][]' id="message_images">`;
-    document.querySelector('form').insertAdjacentHTML('afterend' , html);
-    document.getElementById('new_message').insertAdjacentHTML('beforeend', inputHTML);
+  const changeInput = () => {
+    document.getElementById('message_images').addEventListener('change', function(e){
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      const inputAll = document.getElementsByName('message[images][]');
+      const blobHTML = `<img src="${blob}">`;
+      const inputHTML = `<input type='file' name='message[images][]' id="message_images">`;
+      document.getElementById('new_message').insertAdjacentHTML('beforebegin', blobHTML);
+      document.getElementById('new_message').insertAdjacentHTML('beforeend', inputHTML);
+    });
+  };
+
+  document.getElementById('click-upload').addEventListener('click', function(){
+    clickLabel();
+    changeInput();
   });
 });
