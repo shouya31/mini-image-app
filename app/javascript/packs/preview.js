eb0a3a31d176981ec32a26005dad2365dae75b42
shouya0331 <shouya0331@ezweb.ne.jp>
2020-07-19 15:07:56 +0900

    input要素を追加しましょう

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
   document.querySelector('#message_image'),addEventListener('change', function(e){
-    const image_content = document.querySelector('img');
+  const image_content = document.querySelector('img');
     if (image_content){
       image_content.remove();
     };
@@ -8,6 +8,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const preview = e.target.files[0];
     const blob = window.URL.createObjectURL(preview);
     const html = `<img src="${blob}">`;
+    const inputHTML = `<input type='file' name='message[images][]' id="message_images">`;
     document.querySelector('form').insertAdjacentHTML('afterend' , html);
+    document.getElementById('new_message').insertAdjacentHTML('beforeend', inputHTML);
   });
 });
